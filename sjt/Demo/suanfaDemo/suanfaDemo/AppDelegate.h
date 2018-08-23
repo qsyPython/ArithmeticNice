a4506f2826813c8b675af990a76ea4dad27bdeaf
sjt <fenglingcao@7lk.com>
2018-08-23 17:05:09 +0800

    提交项目

---
+++ sjt/Demo/suanfaDemo/suanfaDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  suanfaDemo
+//
+//  Created by sjt on 2018/8/23.
+//  Copyright © 2018年 sjt. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
