59abf4360fb59aa4aa0a290bd9068488b7565f1e
柴志勇 <jinju@7lk.com>
2018-08-29 19:22:01 +0800

    修改

--- czy/L4/find.py
+++ czy/L4/find.py
@@ -66,26 +66,41 @@ def openDb():
     path = os.path.join(BASE_DIR, 'db.sqlite3')
     mydb = sqlite3.connect(path)
     cursor = mydb.cursor()
-    #不存在则创建表 id 自增型
-    # drop_and_create_table_sql = 'CREATE TABLE IF NOT EXISTS world (id INTEGER PRIMARY KEY NOT NULL,name varchar(100) NOT NULL,continent varchar(100) NOT NULL , \
-    #                              area INTEGER DEFAULT NULL , \
-    #                              population INTEGER DEFAULT NULL , \
-    #                              gdp INTEGER DEFAULT NULL)'
-    #
-    # cursor.execute(drop_and_create_table_sql)
-#插入数据
-    insert_sql = "INSERT INTO world (id,name,continent,area,population,gdp) VALUES (NULL,?,?,?,?,?)"
-    cursor.execute(insert_sql,("china","china1",960,13,10))
-
-    # data = [ (1, ' tom', '男', '广东省广州市', ),
-    #          (2, 'Tom', '男', 22, '美国旧金山',),
-    #          (3, 'Jake', '女', 18, '广东省广州市',),
-    #          (4, 'Cate', '女', 21, '广东省广州市',)]
 
 
-    # cursor.execute("SELECT name,population,area FROM word WHERE area > 3000000 or population > 25500000;")
-    # rows = cursor.fetchall()
-    #print(rows)
+    # #不存在则创建表 id 自增型
+    create_table_sql = 'CREATE TABLE IF NOT EXISTS world (id INTEGER DEFAULT NULL,name varchar(100) NOT NULL,continent varchar(100) NOT NULL , \
+                                 area INTEGER DEFAULT NULL , \
+                                 population INTEGER DEFAULT NULL , \
+                                 gdp INTEGER DEFAULT NULL)'
+    cursor.execute(create_table_sql)
+
+    # 删除数据
+    drop_table_sql = 'DELETE FROM world'
+    cursor.execute(drop_table_sql)
+    mydb.commit()
+
+    #插入数据
+    insert_sql = "INSERT INTO world (id,name,continent,area,population,gdp) VALUES (?,?,?,?,?,?)"
+    for i in  range(5):
+        cursor.execute(insert_sql,(i,'china','chinachina',9600,130,1000))
+        mydb.commit()
+
+    data = [ ('Afghanistan','Asia',652230,25500100,20343000,0),
+             ('Albania', 'Europe', 28748,2831741,12960000,1),
+             ('Algeria', 'Aftica', 2381741,37100000,188681000,2),
+             ('Andorra', 'Europe', 468,78115,3712000,3),
+             ('Angola', 'Africa', 1246700, 20609294,100990000,4)]
+
+    #更新数据
+    for item in data:
+        update_sql = "UPDATE world SET name = ?,continent = ?,area = ?,population =?,gdp = ? WHERE id = ?"
+        cursor.execute(update_sql,item)
+        mydb.commit()
+    #查询数据
+    cursor.execute("SELECT name,population,area FROM world WHERE area > 3000000 or population > 25500000;")
+    rows = cursor.fetchall()
+    print(rows)
 
 def main():
     #gemAndStone("Ha","HHAssaHjj")
