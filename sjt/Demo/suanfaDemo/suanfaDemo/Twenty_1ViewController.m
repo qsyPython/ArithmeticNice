04e2c085ddc019dedf18ceea3b3b286737515d86
sjt <fenglingcao@7lk.com>
2018-09-19 15:12:35 +0800

    作业20-1、20-2

---
+++ sjt/Demo/suanfaDemo/suanfaDemo/Twenty_1ViewController.m
@@ -0,0 +1,156 @@
+//
+//  Twenty_1ViewController.m
+//  suanfaDemo
+//
+//  Created by sjt on 2018/9/19.
+//  Copyright © 2018年 sjt. All rights reserved.
+/*****
+ 给定一个正整数，输出它的补数。补数是对该数的二进制表示取反。
+ 
+ 注意:
+ 
+ 给定的整数保证在32位带符号整数的范围内。
+ 你可以假定二进制数不包含前导零位。
+ 示例 1:
+ 
+ 输入: 5
+ 输出: 2
+ 解释: 5的二进制表示为101（没有前导零位），其补数为010。所以你需要输出2。
+ 示例 2:
+ 
+ 输入: 1
+ 输出: 0
+ 解释: 1的二进制表示为1（没有前导零位），其补数为0。所以你需要输出0。
+ 
+ 算法加餐：
+ 3维数组的表示
+ 输入：给定2个3维数组，按照对应位置获取拿到对应和的值，不同位置的补零处理，并输出一个新的3维数组
+
+
+ */
+
+#import "Twenty_1ViewController.h"
+
+@interface Twenty_1ViewController ()
+
+@end
+
+@implementation Twenty_1ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    NSInteger index=1;
+    NSMutableArray * array=[NSMutableArray arrayWithCapacity:0];
+    [self loadNumber:index withArray:array];//得到倒数
+    NSLog(@"\n作业1：\n输入:%ld\n输出:%ld",index,[self loadBushuNumWithArray:array]);
+    NSArray * threeArray1=@[@[@[@"1"],@[@"2"],@[@"3"]],
+                          @[@[@"4"],@[@"5"],@[@"6"]],
+                          @[@[@"7"],@[@"8"],@[@"9"]]];
+    NSArray * threeArray2=@[@[@[@"11"],@[@"22"],@[@"33"]],
+                          @[@[@"44"],@[@"55"],@[@"66"]],
+                          @[@[@"77"],@[@"88"],@[@"99"]]];
+    
+    NSMutableArray * threeArray=[self sendThreeArray1:threeArray1 withArray2:threeArray2];
+    NSLog(@"结果:%@\n",[threeArray description]);
+}
+
+- (NSInteger)loadBushuNumWithArray:(NSMutableArray *)array{
+    NSInteger sum = 0;
+    NSInteger zhishu = 0;
+    NSInteger num = 0;
+    for (NSInteger i = array.count-1; i>=0; i--) {
+        num = [array[i] integerValue]*pow(2, zhishu);
+        sum+=num;
+        zhishu++;
+    }
+    return sum;
+}
+
+- (void)loadNumber:(NSInteger)index withArray:(NSMutableArray *)array{
+    if (index==0) {
+        return;
+    }
+    NSInteger shang = index/2;
+    NSInteger yushu = index%2;
+    if (yushu==1) {
+        yushu=0;
+    }else{
+        yushu=1;
+    }
+    [array addObject:[NSString stringWithFormat:@"%ld",yushu]];
+    [self loadNumber:shang withArray:array];
+}
+
+- (NSMutableArray *)sendThreeArray1:(NSArray *)array1 withArray2:(NSArray *)array2{
+    NSMutableArray * tempArray=[NSMutableArray arrayWithCapacity:0];
+    NSInteger max = array1.count>array2.count?array1.count:array2.count;
+    NSInteger twoMax = 0;
+    NSInteger threeMax =0;
+    NSInteger num = 0;
+    for (NSInteger i=0; i<max; i++) {
+        NSMutableArray * twoTempArray=[NSMutableArray arrayWithCapacity:0];
+        NSMutableArray * twoarray1=nil;
+        NSMutableArray * twoarray2=nil;
+        twoMax=0;
+        if (i<array1.count) {
+            twoarray1=array1[i];
+            twoMax=twoarray1.count;
+        }
+        
+        if (i<array2.count) {
+            twoarray2=array2[i];
+            if (twoarray2.count>twoMax) {
+                twoMax=twoarray2.count;
+            }
+        }
+        
+        for (NSInteger j =0 ; j<twoMax; j++) {
+            threeMax=0;
+            NSMutableArray * threeTempArray=[NSMutableArray arrayWithCapacity:0];
+            NSMutableArray * threearray1=nil;
+            NSMutableArray * threearray2=nil;
+            if (i<twoarray1.count) {
+                threearray1=twoarray1[j];
+                threeMax=threearray1.count;
+            }
+            
+            if (i<twoarray2.count) {
+                threearray2=twoarray2[j];
+                if (threearray2.count>threeMax) {
+                    threeMax=threearray2.count;
+                }
+            }
+            for (NSInteger k = 0; k<threeMax; k++) {
+                num =0;
+                if (k<threearray1.count) {
+                    num=[threearray1[k] integerValue];
+                }
+                if (k<threearray2.count) {
+                     num+=[threearray2[k] integerValue];
+                }
+                [threeTempArray addObject:[NSString stringWithFormat:@"%ld",num]];
+            }
+            [twoTempArray addObject:threeTempArray];
+        }
+        [tempArray addObject:twoTempArray];
+    }
+    
+    return tempArray;
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
--- sjt/yanghuisanjiao/yanghuisanjiao/main.m
+++ sjt/yanghuisanjiao/yanghuisanjiao/main.m
@@ -10,18 +10,105 @@
 #import <objc/runtime.h>
 #import <stdlib.h>
 
+void Exchange(int **pj, int tr, int tc, int n) {
+    n++;
+    if (1 <= tr && tr <= n / 2 && 1 <= tc && tc <= n) {
+        pj[tr][tc] += pj[n - tr][n - tc];
+        pj[n - tr][n - tc] = pj[tr][tc] - pj[n - tr][n - tc];
+        pj[tr][tc] -= pj[n - tr][n - tc];
+    }
+}
+
+void sendNum(int num){
+    int n[num][num];
+  
+    int count = num*num;
+    int i,j;//old坐标
+    int x,y;
+    if (num%2==1) {
+        i=1;j=num/2+1;
+        n[i][j]=1;
+        for (int k =2; k<=count+1; k++) {
+            x=i-1;
+            y=j+1;
+            if (x<1) {
+                x=num;
+            }
+            
+            if (y>num) {
+                y=1;
+            }
+            
+            if (0<n[x][y]&&n[x][y]<=count) {
+                x=i+1;
+                y=j;
+                if (x<1) {
+                    x=num;
+                }
+            }
+            n[x][y]=k;
+            i=x;
+            j=y;
+            
+        }
+        for (i = 1; i <= num; i++) {
+            for (j = 1; j <= num; j++)
+                printf("%d\t",n[i][j]);
+            printf("\n");
+        }
+    }else{
+        int **pj;
+        pj = (int**)malloc(sizeof(int **) * (num + 1));
+        for (i = 0; i < (num + 1); i++)
+            pj[i] = (int*)malloc(sizeof(int *) *  (num + 1));
+        
+        if (num % 4 == 0) {
+            i = 1; j = 1;
+            // 1.先将数据从上到下，从左到右填入
+            for (int k = 1; k <= num * num; k++) {
+                pj[i][j++] = k;
+                if (j > num) { j = 1; i++; }
+            }
+            
+            // 2.将方阵的所有4*4子方阵中的两对角线上的数
+            // 关于大方阵中心作中心对称交换
+            
+            i = 1; j = 1;
+            for (int r = 0; r < num / 4 + 1; r++) {
+                for (int c = 0; c < num / 4 + !(r % 2); c++) {
+                    
+                    x = 2 * r + i;
+                    y = 4 * c + r % 2 * 2 + j;
+                    
+                    Exchange(pj, x, y, num);
+                    Exchange(pj, x - 1, y, num);
+                    Exchange(pj, x, y - 1, num);
+                    Exchange(pj, x - 1, y - 1, num);
+                }
+            }
+            for (i = 1; i <= num; i++) {
+                for (j = 1; j <= num; j++)
+                    printf("%d\t",pj[i][j]);
+                printf("\n");
+            }
+        }
+    }
+    
+    
+}
+
 void zifuchuan(int num , int max){
     
     unsigned long numLenth= 0;
     
     if (num>0) {
-         printf("%d",num);
+        printf("%d",num);
         while (num != 0)
         {
             numLenth++;
             num /= 10;
         }
-       
+        
     }else{
         numLenth=0;
     }
@@ -32,93 +119,60 @@ void zifuchuan(int num , int max){
     }
 }
 
-void sendNum(int num){
-    num =5;
-    int n[num][num];
-    int count = num*num;
-    int i,j;//old坐标
-    int ii,jj;
-    if (num%2==1) {
-        i=1;j=num/2+1;
-        n[i][j]=1;
-        for (int k =2; k<=count; k++) {
-            ii=i-1;
-            jj=j+1;
-            if (ii<1) {
-                ii=num;
-            }
-            if (jj>num) {
-                jj=1;
-            }
-            if (0<n[ii][jj]&&n[ii][jj]<=count) {
-                ii=i+1;
-                jj=j;
-                if (ii<0) {
-                    ii=num;
-                }
-            }
-            n[ii][jj]=k;
-            i=ii;
-            j=jj;
-            
-        }
-    }else{
-        
-    }
-    for (i = 1; i <= num; i++) {
-        for (j = 1; j <= num; j++)
-            printf("%d\t",n[i][j]);
-        printf("\n");
+void yanghuisanjiao(int num){
+    int count = num;
+    int sum = 0;
+    int n[count+1][2*count+1];
+    int maxNum= pow(2, count-1);
+    int maxcount=0;
+    while (maxNum != 0)
+    {
+        maxcount++;
+        maxNum /= 10;
     }
     
+    for (int i = 0; i<count+1; i++) {
+        for (int j =0 ; j<2*count+1; j++) {
+            if ((j==count-i)||(j==count+i)) {
+                n[i][j]=1;
+                zifuchuan(1, maxcount);
+            }else{
+                if (i-1>0) {
+                    sum =0;
+                    if (j-1>=0) {
+                        sum = n[i-1][j-1];
+                    }
+                    
+                    if (j+1<(2*count+1)) {
+                        sum+= n[i-1][j+1];
+                    }
+                    n[i][j]=sum;
+                    zifuchuan(sum, maxcount);
+                }else
+                {
+                    n[i][j]=0;
+                    zifuchuan(0, maxcount);
+                }
+            }
+        }
+        printf("\n");
+        
+    }
 }
 
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
-        int count = 9;
-        int sum = 0;
-        int n[count+1][2*count+1];
-        int maxNum= pow(2, count-1);
-        int maxcount=0;
-        while (maxNum != 0)
-        {
-            maxcount++;
-            maxNum /= 10;
-        }
-        
-        for (int i = 0; i<count+1; i++) {
-            for (int j =0 ; j<2*count+1; j++) {
-                if ((j==count-i)||(j==count+i)) {
-                    n[i][j]=1;
-                    zifuchuan(1, maxcount);
-                }else{
-                    if (i-1>0) {
-                        sum =0;
-                        if (j-1>=0) {
-                            sum = n[i-1][j-1];
-                        }
-                        
-                        if (j+1<(2*count+1)) {
-                            sum+= n[i-1][j+1];
-                        }
-                        n[i][j]=sum;
-                        zifuchuan(sum, maxcount);
-                    }else
-                    {
-                        n[i][j]=0;
-                        zifuchuan(0, maxcount);
-                    }
-                }
-            }
-            printf("\n");
-            
-        }
-       
+        printf("魔方矩阵\n");
         sendNum(5);
+        printf("杨辉三角\n");
+        yanghuisanjiao(5);
+        
         
     }
     return 0;
 }
+
+
 
 
 
