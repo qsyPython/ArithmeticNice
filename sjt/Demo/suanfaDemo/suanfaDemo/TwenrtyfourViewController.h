441e8bd2123fa56fcbd963e6dcbe9213902c25fe
sjt <fenglingcao@7lk.com>
2018-09-26 17:18:57 +0800

    作业21-1  24-2

---
+++ sjt/Demo/suanfaDemo/suanfaDemo/TwenrtyfourViewController.h
@@ -0,0 +1,13 @@
+//
+//  TwenrtyfourViewController.h
+//  suanfaDemo
+//
+//  Created by sjt on 2018/9/26.
+//  Copyright © 2018年 sjt. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TwenrtyfourViewController : UIViewController
+
+@end
