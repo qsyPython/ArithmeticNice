cafa8fa5e2bf72eef07462b8ee740b24ea7e180c
midiexiang <midiexiang@7lk.com>
2018-10-11 17:38:54 +0800

    5种传值介绍

---
+++ qsy/interview/4_five_pass_value.py
@@ -0,0 +1,9 @@
+'''
+    传值涉及到从前到后，从后到前
+    都可以使用:5种
+    property
+    delegate:A中声明协议，声明代理属性；B中遵守协议，执行代理方法
+    singleton：单例中持有某属性
+    notification:注册给某对象；发布通知执行就ok
+    block:A中重命名block，声明block属性，定义block；B中执行block
+'''
