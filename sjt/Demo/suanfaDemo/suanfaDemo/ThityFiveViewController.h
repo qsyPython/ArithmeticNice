025cfefffc92b31f6b7b7ce8f47c39b9eb5a46f5
sjt <fenglingcao@7lk.com>
2018-10-19 19:47:17 +0800

    作业35-1

---
+++ sjt/Demo/suanfaDemo/suanfaDemo/ThityFiveViewController.h
@@ -0,0 +1,13 @@
+//
+//  ThityFiveViewController.h
+//  suanfaDemo
+//
+//  Created by sjt on 2018/10/19.
+//  Copyright © 2018年 sjt. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ThityFiveViewController : UIViewController
+
+@end
