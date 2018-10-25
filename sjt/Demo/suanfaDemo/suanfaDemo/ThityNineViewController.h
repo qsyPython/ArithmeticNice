04e5f05946034c6104fee2c4e7518e1a1dbcc81c
sjt <fenglingcao@7lk.com>
2018-10-25 19:38:41 +0800

    作业39-1

---
+++ sjt/Demo/suanfaDemo/suanfaDemo/ThityNineViewController.h
@@ -0,0 +1,13 @@
+//
+//  ThityNineViewController.h
+//  suanfaDemo
+//
+//  Created by sjt on 2018/10/25.
+//  Copyright © 2018年 sjt. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ThityNineViewController : UIViewController
+
+@end
